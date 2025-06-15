d82506f6bd3511aea017cf1faf838f8700a430ec
grasbiel <bzsilva1998@gmail.com>
2025-06-15 15:05:31 -0300

    Correção de transactions

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -196,7 +196,7 @@ export default function Transactions() {
                 component="div"
                 count={totalCount}
                 page={page}
-                onPageChange={(e, newPage) => setPage(newPage)}
+                onPageChange={(_, newPage) => setPage(newPage)}
                 rowsPerPage={rowsPerPage}
                 onRowsPerPageChange={e => {
                 setRowsPerPage(parseInt(e.target.value, 10));
