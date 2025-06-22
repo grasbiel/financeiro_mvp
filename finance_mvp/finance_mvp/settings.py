b4991a6b6da213bb81ac8d9774ca532c08d0087f
grasbiel <bzsilva1998@gmail.com>
2025-06-22 15:35:06 -0300

    Correção de arquivos para deploy

--- finance_mvp/finance_mvp/settings.py
+++ finance_mvp/finance_mvp/settings.py
@@ -51,15 +51,18 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 REST_FRAMEWORK = {
@@ -148,3 +151,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
