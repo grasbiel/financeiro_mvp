335f764c9ede3aa12d788eaab92ae3811e4dbdf2
grasbiel <bzsilva1998@gmail.com>
2025-07-06 22:04:33 -0300

    Edição de roteamento

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-    baseURL: baseURL
+    baseURL: baseURL || "/api"
 });
 
 api.interceptors.request.use(cfg => {
