d89c88b73825a44d54543bbed5762767def2db36
grasbiel <bzsilva1998@gmail.com>
2025-07-11 23:23:22 -0300

    Adição de configuração do api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,8 +2,14 @@ import axios from 'axios'
 
 const baseURL = import.meta.env.VITE_API_URL
 
+if (!baseURL && import.meta.env.DEV){
+    console.error(
+        'A variável de ambiente VITE_API_URL não está definida' +
+        'Crie um arquivo .env.local e defina VITE_API_URL=http://localhost:8000/api'
+    )
+}
 const api = axios.create({
-    baseURL: baseURL || "/api"
+    baseURL: baseURL,
 });
 
 api.interceptors.request.use(cfg => {
